94f57c400b2f28478406774cdccbd9ba6603c4ee
hedgehogz <sheldonwhung@gmail.com>
2025-03-08 01:20:22 -0500

    link test

---
+++ events.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var button = document.getElementById("LinkedInButton");
+
+    button.addEventListener("click", clicked);
+})
+
+function clicked() {
+    window.open("https://www.linkedin.com/in/sheldon-hung-384334243/");
+}
