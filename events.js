221de2674ed091fe23799805fec3264f3acbbe78
hedgehogz <sheldonwhung@gmail.com>
2025-03-15 18:47:46 -0400

    form test

--- events.js
+++ events.js
@@ -1,20 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    const project1Open = document.getElementById("popupOpenProject1");
-    const project2Open = document.getElementById("popupOpenProject2");
-    const project3Open = document.getElementById("popupOpenProject3");
+    const mailOpen = document.getElementById("mailOpen");
+    const mailClose = document.getElementById("mailClose");
+    const mailDisplay = document.getElementById("mailSection")
 
-    const project1Close = document.getElementById("popupCloseProject1");
-    const project2Close = document.getElementById("popupCloseProject2");
-    const project3Close = document.getElementById("popupCloseProject3");
+    const sendEmail = document.getElementById("sendEmail");
 
-    const project1Display = document.getElementById("popupProject1")
-    const project2Display = document.getElementById("popupProject2")
-    const project3Display = document.getElementById("popupProject3")
-
-    const openButtons = [project1Open, project2Open, project3Open];
-    const closeButtons = [project1Close, project2Close, project3Close];
-    const projectDisplays = [project1Display, project2Display, project3Display];
+    const openButtons = [mailOpen];
+    const closeButtons = [mailClose, sendEmail];
+    const projectDisplays = [mailDisplay, mailDisplay];
 
     function togglePopup(element, state) {
         element.classList.toggle(state)
