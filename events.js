5de11f81eeb76e1ee1e723d620c7fa476fc67cd8
hedgehogz <sheldonwhung@gmail.com>
2025-03-15 16:05:37 -0400

    update projects

--- events.js
+++ events.js
@@ -1,13 +1,36 @@
-/*
 document.addEventListener("DOMContentLoaded", function() {
-    const linkedinButton = document.getElementById("LinkedInButton");
-    const githubButton = document.getElementById("GithubButton");
 
-    function clicked() {
-        window.open();
+    const project1Open = document.getElementById("popupOpenProject1");
+    const project2Open = document.getElementById("popupOpenProject2");
+    const project3Open = document.getElementById("popupOpenProject3");
+
+    const project1Close = document.getElementById("popupCloseProject1");
+    const project2Close = document.getElementById("popupCloseProject2");
+    const project3Close = document.getElementById("popupCloseProject3");
+
+    const project1Display = document.getElementById("popupProject1")
+    const project2Display = document.getElementById("popupProject2")
+    const project3Display = document.getElementById("popupProject3")
+
+    const openButtons = [project1Open, project2Open, project3Open];
+    const closeButtons = [project1Close, project2Close, project3Close];
+    const projectDisplays = [project1Display, project2Display, project3Display];
+
+    function togglePopup(element, state) {
+        element.classList.toggle(state)
     }
 
-    linkedinButton.addEventListener("click", clicked);
-    githubButton.addEventListener("click", clicked);
+    for (let i=0; i<openButtons.length; i++) {
+        openButtons[i].addEventListener("click", function() {
+            togglePopup(projectDisplays[i], "show");
+        });
+    }
 
-})*/
+    for (let i=0; i<closeButtons.length; i++) {
+        closeButtons[i].addEventListener("click", function() {
+            togglePopup(projectDisplays[i], "show");
+        });
+    }
+
+});
+
