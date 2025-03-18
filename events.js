24284cd1fc3350a40c931fc3c68edc933c434f1a
hedgehogz <sheldonwhung@gmail.com>
2025-03-18 00:36:38 -0400

    brain hurts

--- events.js
+++ events.js
@@ -1,42 +1,42 @@
-document.addEventListener("DOMContentLoaded", function() {
+// document.addEventListener("DOMContentLoaded", function() {
 
-    const mailOpen = document.getElementById("mailOpen");
-    const mailClose = document.getElementById("mailClose");
-    const confirmationClose = document.getElementById("confirmationClose");
+const mailOpen = document.getElementById("mailOpen");
+const mailClose = document.getElementById("mailClose");
+const confirmationClose = document.getElementById("confirmationClose");
 
-    const mailDisplay = document.getElementById("mailSection")
-    const confirmationDisplay = document.getElementById("confirmationDisplay")
+const mailDisplay = document.getElementById("mailSection")
+const confirmationDisplay = document.getElementById("confirmationDisplay")
 
-    const form = document.getElementById("googleForm");
+const form = document.getElementById("googleForm");
 
-    const openButtons = [mailOpen];
-    const closeButtons = [mailClose, confirmationClose];
-    const projectDisplays = [mailDisplay, confirmationDisplay];
+const openButtons = [mailOpen];
+const closeButtons = [mailClose, confirmationClose];
+const projectDisplays = [mailDisplay, confirmationDisplay];
 
-    function togglePopup(element, state) {
-        element.classList.toggle(state)
-    }
+function togglePopup(element, state) {
+    element.classList.toggle(state)
+}
 
-    function submitEvent() {
-        togglePopup(confirmationDisplay, "show");
-        console.log('done');
-    }
+function submitEvent(e) {
+    togglePopup(confirmationDisplay, "show");
+    console.log('done');
+}
 
-    function hideElement(array) {
-        for (let i=0; i<array.length; i++) {
-            if (array[i] != null) {
-                array[i].addEventListener("click", function() {
-                    togglePopup(projectDisplays[i], "show");
-                    form.reset();
-                });
-            }
+function hideElement(array) {
+    for (let i=0; i<array.length; i++) {
+        if (array[i] != null) {
+            array[i].addEventListener("click", function() {
+                togglePopup(projectDisplays[i], "show");
+                form.reset();
+            });
         }
     }
+}
 
-    hideElement(openButtons);
-    hideElement(closeButtons);
+hideElement(openButtons);
+hideElement(closeButtons);
 
-    form.addEventListener("submit", submitEvent);
+form.addEventListener("submit", submitEvent);
 
-});
+// });
 
